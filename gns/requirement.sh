9111f8df141eea1749ae126f41b7c8c07b54d00c
CHNmyx <2602089875@qq.com>
2025-06-25 19:53:55 +0800

    gns

---
+++ gns/requirement.sh
@@ -0,0 +1,7 @@
+pip install torch==2.4.0
+pip install torch-scatter -f "https://pytorch-geometric.com/whl/torch-2.4.0+cu121.html"
+pip install torch-sparse -f "https://pytorch-geometric.com/whl/torch-2.4.0+cu121.html"
+pip install torch-geometric
+pip install -q git+https://github.com/snap-stanford/deepsnap.git
+pip install torch-cluster -f https://data.pyg.org/whl/torch-2.4.0+cu121.html
+pip install absl-py
